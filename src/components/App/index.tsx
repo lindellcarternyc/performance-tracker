f02889d23b0a556bd033682aaabf3c441c47fbaa
Lindell Carter <lindellcarternyc@gmail.com>
2018-10-14 15:46:18 -0400

    add store and router to app

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Routes from 'routes'
+
+import { Provider } from 'react-redux'
+import initStore from 'store'
 
 import { ThemeProvider } from 'styles/styled-components'
 import { LightTheme } from 'styles/themes'
 
-import Calendar from 'components/Calendar'
+// import Calendar from 'components/Calendar'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
 import Main from 'components/Main'
@@ -14,6 +20,8 @@ import gigService from 'services/gig.service'
 interface AppState {
   gigs: GigModel[]
 }
+
+const store = initStore()
 
 class App extends React.Component<{}, AppState> {
   public readonly state: AppState = {
@@ -27,25 +35,30 @@ class App extends React.Component<{}, AppState> {
 
   public render() {
     return (
-      <ThemeProvider theme={LightTheme}>
-        <div>
-          <Header />
-          <Main>
-            <Calendar gigs={this.state.gigs} onSelectGig={this.onSelectGig} />
-          </Main>
-          <Footer />
-        </div>
-      </ThemeProvider>
+      <Provider store={store}>
+        <Router>
+          <ThemeProvider theme={LightTheme}>
+            <div>
+              <Header />
+              <Main>
+                <Routes />
+                {/* <Calendar gigs={this.state.gigs} onSelectGig={this.onSelectGig} /> */}
+              </Main>
+              <Footer />
+            </div>
+          </ThemeProvider>
+        </Router>
+      </Provider>
     )
   }
 
-  private onSelectGig = (gig: GigModel, evt: React.SyntheticEvent) => {
-    evt.preventDefault()
-    evt.stopPropagation()
+  // private onSelectGig = (gig: GigModel, evt: React.SyntheticEvent) => {
+  //   evt.preventDefault()
+  //   evt.stopPropagation()
 
-    // tslint:disable-next-line:no-console
-    console.log(gig)
-  }
+  //   // tslint:disable-next-line:no-console
+  //   console.log(gig)
+  // }
 }
 
 export default App
