b57059c98ef3bdb0386b39b147cb0e7ea6aa325f
Lindell Carter <lindellcarternyc@gmail.com>
2018-10-15 12:31:01 -0400

    add createGig to service

--- src/services/gig.service.ts
+++ src/services/gig.service.ts
@@ -1,7 +1,16 @@
 import gigData from 'database/gig.data'
 
+import { parseNewGigModel } from 'factories/gig.factory'
+import { GigModel, NewGigModel } from 'models/gig.model'
+
 const gigs = () => gigData.getGigs()
 
+const createGig = async (newGig: NewGigModel): Promise<GigModel> => {
+  const gigModel = parseNewGigModel(newGig)
+  return gigData.addGig(gigModel)
+}
+
 export default {
-  gigs
+  gigs,
+  createGig
 }
