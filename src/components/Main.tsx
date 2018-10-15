3b2cc3389f15badcd5081b08cdc58b0aa4fd3199
Lindell Carter <lindellcarternyc@gmail.com>
2018-10-15 12:29:19 -0400

    update styles

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@ import styled from 'styles/styled-components'
 
 const Main = styled.main`
   padding-top: 55px;
-  padding-bottom: 35px;
+  padding-bottom: 55px;
   padding-left: 1rem;
   padding-right: 1rem;
   height: 100vh;
---
+++ src/components/Title.ts
@@ -0,0 +1,8 @@
+import styled from 'styles/styled-components'
+
+const Title = styled.h3`
+  color: ${({ theme }) => theme.accent};
+  text-align: center;
+`
+
+export default Title
