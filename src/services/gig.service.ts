11f24e674d1fc16070d87d7df15778f1cbf4263a
Lindell Carter <lindellcarternyc@gmail.com>
2018-10-16 14:06:04 -0400

    use db in service

--- src/services/gig.service.ts
+++ src/services/gig.service.ts
@@ -1,16 +1,80 @@
-import gigData from 'database/gig.data'
+// import gigData from 'database/gig.data'
 
 import { parseNewGigModel } from 'factories/gig.factory'
-import { GigModel, NewGigModel } from 'models/gig.model'
+import { GigModel,
+  //  GigModelCollection, 
+   GigModelCollection, 
+   NewGigModel} from 'models/gig.model'
 
-const gigs = () => gigData.getGigs()
+import Database from 'database/firebase'
+import { DatabaseCollection, DatabaseModel } from 'database/types'
 
-const createGig = async (newGig: NewGigModel): Promise<GigModel> => {
+import * as moment from 'moment'
+
+const parseDate = (input: string) => moment(input).toDate()
+const deserializeGig = (databaseGig: DatabaseModel<GigModel>): GigModel => {
+  const { id, title, location } = databaseGig
+  const start = parseDate(databaseGig.start)
+  const end = parseDate(databaseGig.end)
+  const pay = parseInt(databaseGig.pay, 10)
+
+  return {
+    id, title, pay, location,
+    start, end
+  }
+}
+const serializeGig = (gig: GigModel): DatabaseModel<GigModel> => {
+  const { title, location, id } = gig
+  const pay = gig.pay.toString()
+  const start = gig.start.toString()
+  const end = gig.end.toString()
+
+  return {
+    title,
+    id,
+    location,
+    pay,
+    start,
+    end
+  }
+}
+
+const db = new Database()
+
+const deserialiazeGigCollection = (collection: DatabaseCollection<GigModel>): GigModelCollection => {
+  return Object.keys(collection).reduce((acc, id) => {
+    return { ...acc, [id]: deserializeGig(collection[id]) }
+  }, { })
+}
+
+const subscribeToGigs = (cb: (gigs: GigModelCollection) => void) => {
+  db.subscribe('gigs', collection => {
+    const deserialized = deserialiazeGigCollection(collection)
+    cb(deserialized)
+  })
+}
+
+const gigs = async (): Promise<{[id: string]: GigModel}> => {
+  return db.getGigs<GigModel>()
+  .then(dbGigModels => {
+    const gigCollection = Object.keys(dbGigModels).reduce((acc, id) => {
+      const gig = deserializeGig(dbGigModels[id])
+      return { ...acc, [gig.id]: gig }
+    }, { })
+    // tslint:disable-next-line:no-console
+    return gigCollection
+  })
+}
+
+const createGig = (newGig: NewGigModel)  => {
   const gigModel = parseNewGigModel(newGig)
-  return gigData.addGig(gigModel)
+  const serialized = serializeGig(gigModel)
+  db.create('gig', serialized)
+  return gigModel
 }
 
 export default {
   gigs,
-  createGig
+  createGig,
+  subscribeToGigs
 }
